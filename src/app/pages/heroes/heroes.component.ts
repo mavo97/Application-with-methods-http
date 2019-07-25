de45550bccd734925693a216cf929d50967f15cb
mavo97 <mavodeveloper@gmail.com>
2019-07-25 16:01:07 -0500

    Application finishes

---
+++ src/app/pages/heroes/heroes.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from 'src/app/services/heroes.service';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: HeroeModel[] = [];
+  cargando = false;
+
+  constructor( private heroesService: HeroesService ) { }
+
+  ngOnInit() {
+
+    this.cargando = true;
+    this.heroesService.getHeroes()
+    .subscribe(resp => {
+      this.heroes = resp;
+      this.cargando = false;
+    });
+  }
+
+  borrarHeroe( heroe: HeroeModel, i: number ) {
+
+    Swal.fire({
+      title: '¿Esta seguro?',
+      text: `¿Esta seguro de borrar a ${heroe.nombre}`,
+      type: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(
+      resp => {
+        if ( resp.value ) {
+          this.heroes.splice(i, 1);
+          this.heroesService.borrarHeroe(heroe.id).subscribe();
+        }
+      }
+    );
+  }
+}
